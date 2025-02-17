32797f78ecf581298c3d9adcaf8b4a663b7dea7d
TonyP1992 <okpetrakov@gmail.com>
2025-02-17 19:48:40 +0800

    add items

--- js/task-1.js
+++ js/task-1.js
@@ -1,14 +1,15 @@
-// Створіть масив styles з елементами 'jazz' і 'blues'
-// Додайте до кінця масиву елемент 'rock-n-roll' за допомогою відповідного методу масивів
-// Знайдіть елемент 'blues' у масиві та замініть його на 'classic', використовуючи код
-
-// Напишіть функцію logItems(array), яка приймає масив як аргумент
-// і виводить у консоль кожен його елемент у форматі:
-// "<номер елемента> - <значення елемента>".
-// Використайте цикл for для перебору елементів масиву.
-// Нумерація елементів повинна починатися з 1 (а не з 0).
-
-const styles = ["jazz" , 'blues'];
-styles.push('rock-n-roll');
-console.log(styles);
-console.log(styles.indexOf('blues'));
+function isEnoughCapacity(products, containerSize) {
+    let quantities = Object.values(products);
+    
+    let total = 0;
+    for (let quantity of quantities) {
+      total += quantity;
+    }
+    
+    return total <= containerSize;
+  }
+  
+  console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); 
+  console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); 
+  console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); 
+  console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); 
---
+++ js/task-3.js
@@ -0,0 +1,27 @@
+// const profile = {
+//     username: "Jacob",
+//     playTime: 300
+// };
+
+// function changeUsername(profile, newName) {
+//     profile.username = newName; 
+// }
+
+
+// function updatePlayTime(profile, hours) {
+//     profile.playTime += hours; 
+// }
+
+
+// function getInfo(profile) {
+//     return profile.username + " has " + profile.playTime + " active hours!"; 
+// }
+
+
+// console.log(getInfo(profile));
+
+// changeUsername(profile, "Marco"); 
+// console.log(getInfo(profile)); 
+
+// updatePlayTime(profile, 20); 
+// console.log(getInfo(profile)); 
