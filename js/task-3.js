970ff290c1dd14e50c98c9d9ec74d0e8de7ad2f7
TonyP1992 <okpetrakov@gmail.com>
2025-02-18 12:11:45 +0800

    add new items

--- js/task-3.js
+++ js/task-3.js
@@ -1,27 +1,27 @@
-// const profile = {
-//     username: "Jacob",
-//     playTime: 300
-// };
+const profile = {
+    username: "Jacob",
+    playTime: 300
+};
 
-// function changeUsername(profile, newName) {
-//     profile.username = newName; 
-// }
-
-
-// function updatePlayTime(profile, hours) {
-//     profile.playTime += hours; 
-// }
+function changeUsername(profile, newName) {
+    profile.username = newName; 
+}
 
 
-// function getInfo(profile) {
-//     return profile.username + " has " + profile.playTime + " active hours!"; 
-// }
+function updatePlayTime(profile, hours) {
+    profile.playTime += hours; 
+}
 
 
-// console.log(getInfo(profile));
+function getInfo(profile) {
+    return profile.username + " has " + profile.playTime + " active hours!"; 
+}
 
-// changeUsername(profile, "Marco"); 
-// console.log(getInfo(profile)); 
 
-// updatePlayTime(profile, 20); 
-// console.log(getInfo(profile)); 
+console.log(getInfo(profile));
+
+changeUsername(profile, "Marco"); 
+console.log(getInfo(profile)); 
+
+updatePlayTime(profile, 20); 
+console.log(getInfo(profile)); 
